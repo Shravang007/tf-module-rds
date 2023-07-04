3055449cd51e142a73bb5fbd03d07785596e2336
Shravang007 <shravangoudgajula@gmail.com>
2023-07-04 22:47:20 +0530

    docdb

--- main.tf
+++ main.tf
@@ -41,9 +41,8 @@ resource "aws_rds_cluster" "main" {
   storage_encrypted       = true
   kms_key_id              = var.kms_key_arn
   vpc_security_group_ids  = [aws_security_group.sg.id]
-  skip_final_snapshot = true
+  skip_final_snapshot     = true
 
-  final_snapshot_identifier = "DELETE ME"
 }
 
 resource "aws_rds_cluster_instance" "main" {
