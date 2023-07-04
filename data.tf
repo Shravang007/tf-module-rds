94b3ea2c8f334bd14421c8a3e72fd7d581cace12
Shravang007 <shravangoudgajula@gmail.com>
2023-07-04 19:01:08 +0530

    RDS to kms key connect

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 data "aws_ssm_parameter" "username" {
-  name = "roboshop.${var.env}.mysql.amqp.username"
+  name = "roboshop.${var.env}.mysql.amqp_username"
 }
 
 data "aws_ssm_parameter" "password" {
-  name = "roboshop.${var.env}.mysql.amqp.password"
+  name = "roboshop.${var.env}.mysql.amqp_password"
 }
