f1e7836d9bc34eae2c3eb40fa402ea45cf3e4132
Shravang007 <shravangoudgajula@gmail.com>
2023-07-04 22:51:06 +0530

    docdb

--- main.tf
+++ main.tf
@@ -43,6 +43,7 @@ resource "aws_rds_cluster" "main" {
   vpc_security_group_ids  = [aws_security_group.sg.id]
   skip_final_snapshot     = true
 
+  mysql-dev-final-snapshot = "DELETE ME"
 }
 
 resource "aws_rds_cluster_instance" "main" {
