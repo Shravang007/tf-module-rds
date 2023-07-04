9318d1d30a0c0afdaa4ba43503756a156be446bf
Shravang007 <shravangoudgajula@gmail.com>
2023-07-04 11:12:53 +0530

    RDS to kms key connect

--- main.tf
+++ main.tf
@@ -14,7 +14,8 @@ resource "aws_rds_cluster" "default" {
   master_username         = data.aws_ssm_parameter.username.value
   master_password         = data.aws_ssm_parameter.password.value
   db_subnet_group_name = aws_db_subnet_group.main.name
-  kms_key_id = var.kms_key_id
+  storage_encrypted = true
+  kms_key_id = var.kms_key_arn
 }
 
 
